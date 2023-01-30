91ecb9d05075fd3ccb63d0aaf8f14cc762c3adb8
Samarjit97 <samanjitsingh42@gmail.com>
2023-01-30 16:30:24 +0100

    first update

---
+++ app/src/main/java/com/example/vialibrary/Borrower.java
@@ -0,0 +1,33 @@
+package com.example.vialibrary;
+
+public class Borrower {
+   private String name, email,role;
+    int phoneNumber;
+
+    public Borrower(String name, String email, String role, int phoneNumber) {
+        this.name = name;
+        this.email = email;
+        this.role = role;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Borrower() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+}
---
+++ app/src/main/java/com/example/vialibrary/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.vialibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+    EditText name;
+    EditText email;
+    EditText phone;
+    Spinner role;
+    Button submit;
+    DatabaseReference borrowerRef;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        name=findViewById(R.id.name);
+        email=findViewById(R.id.email);
+        phone=findViewById(R.id.phone);
+        role=findViewById(R.id.role);
+        submit=findViewById(R.id.submit);
+
+        borrowerRef= FirebaseDatabase.getInstance().getReference().child("Borrower");
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                insertBorrowerData();
+            }
+
+            private void insertBorrowerData() {
+                String bName=name.getText().toString();
+                String bemail=email.getText().toString();
+                int bPhone=Integer.parseInt(phone.getText().toString());
+                String bRole=role.getSelectedItem().toString();
+
+
+                Borrower borrower=new Borrower(bName,bemail,bRole,bPhone);
+
+                borrowerRef.push().setValue(borrower);
+                Toast.makeText(MainActivity.this, "Data Inserted", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+}
